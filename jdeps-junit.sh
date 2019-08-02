17f7d28aa29d317ea91cf19e435a23ac0e01cfb5
Piotr <piotr.przybyl@vectus.pl>
2019-08-02 13:30:24 +0200

    Added demonstration of jdeps.

---
+++ jdeps-junit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+jdeps libs/junit-4.12.jar libs/hamcrest-core-1.3.jar
+#jdeps -s libs/junit-4.12.jar libs/hamcrest-core-1.3.jar
+#jdeps -v libs/junit-4.12.jar libs/hamcrest-core-1.3.jar
+jdeps --generate-module-info /tmp/dotsummary libs/junit-4.12.jar libs/hamcrest-core-1.3.jar
+jdeps -dotoutput /tmp/dotsummary libs/junit-4.12.jar libs/hamcrest-core-1.3.jar
